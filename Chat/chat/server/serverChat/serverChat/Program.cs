0c676ca66c4b1590739eabe58cfcd2375f14786c
Koval1997 <kovalmax06@gmail.com>
2017-06-06 22:37:46 +0500

    Add files via upload

---
+++ Chat/chat/server/serverChat/serverChat/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace serverChat
+{
+    class Program
+    {
+        private const int _serverPort = 9595;
+        private static Thread _serverThread;
+        static void Main(string[] args)
+        {
+            _serverThread = new Thread(startServer);
+            _serverThread.IsBackground = true;
+            _serverThread.Start();
+            while (true)
+                handlerCommands(Console.ReadLine());
+        }
+        private static void handlerCommands(string cmd)
+        {
+            cmd = cmd.ToLower();
+            if (cmd.Contains("/getusers"))
+            {
+                int countUsers = Server.Clients.Count;
+                for (int i = 0; i < countUsers; i++)
+                {
+                    Console.WriteLine("[{0}]: {1}",i,Server.Clients[i].UserName);
+                }
+            }
+        }
+        private static void startServer()
+        {
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.66");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, _serverPort);
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipEndPoint);
+            socket.Listen(1000);
+            Console.WriteLine("Сервер запщуен с адерсом: {0}.",ipEndPoint);
+            while(true)
+            {
+                try
+                {
+                    Socket user = socket.Accept();
+                    Server.NewClient(user);
+                }
+                catch (Exception exp) { Console.WriteLine("Ошибка: {0}",exp.Message); }
+            }
+
+        }
+    }
+}
